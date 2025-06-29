d6df7b6aaabc73b0df81603a90e2c8dbeb673b34
Jacob Van Order <jacob@sushigrass.com>
2025-06-29 17:53:27 -0500

    Converted ImageURLProtocol to Use Tasks But Also Had to Use `@preconcurrency`.

--- AsynchronouslyLoadingImagesIntoTableAndCollectionViews/Async Image Loading/ImageURLProtocol.swift
+++ AsynchronouslyLoadingImagesIntoTableAndCollectionViews/Async Image Loading/ImageURLProtocol.swift
@@ -4,15 +4,16 @@
  Abstract:
  The ImageURLProtocol of the sample.
  */
+import Foundation
 import UIKit
+import os
 
-// TODO: Convert to Swift 6 Safe Type
-class ImageURLProtocol: URLProtocol {
+@preconcurrency // URLProtocol, introduced with iOS 2.0 is preconcurrency.
+final class ImageURLAsyncProtocol: URLProtocol {
 
-    var cancelledOrComplete: Bool = false
-    var block: DispatchWorkItem!
-
-    private static let queue = DispatchSerialQueue(label: "com.apple.imageLoaderURLProtocol")
+    private var asyncTask: Task<(), Never>?
+    private let logger = Logger(subsystem: "com.example.async-image-loading.ImageURLProtocol",
+                        category: "Loader")
 
     override class func canInit(with request: URLRequest) -> Bool {
         return true
@@ -31,12 +32,15 @@ class ImageURLProtocol: URLProtocol {
             return
         }
 
-
-        block = DispatchWorkItem(block: {
-            if self.cancelledOrComplete == false {
+        self.asyncTask = Task { [weak self] in
+            guard let self else { return }
+            do {
+                try Task.checkCancellation()
+                try await Task.sleep(for: .randomSeconds(min: 0.5, max: 3.0))
+                try Task.checkCancellation()
                 let fileURL = URL(fileURLWithPath: reqURL.path)
-                if let data = try? Data(contentsOf: fileURL),
-                   let httpResponse = HTTPURLResponse(url: reqURL,
+                let data = try Data(contentsOf: fileURL)
+                if let httpResponse = HTTPURLResponse(url: reqURL,
                                                       statusCode: 200,
                                                       httpVersion: nil,
                                                       headerFields: nil) {
@@ -46,29 +50,29 @@ class ImageURLProtocol: URLProtocol {
                     urlClient.urlProtocol(self, didLoad: data)
                     urlClient.urlProtocolDidFinishLoading(self)
                 }
+            } catch {
+                self.logger.debug("Error with load: \(error.localizedDescription)")
+                urlClient.urlProtocol(self, didFailWithError: error)
             }
-            self.cancelledOrComplete = true
-        })
-
-        ImageURLProtocol.queue.asyncAfter(deadline: .now() + 3.0, execute: block)
+        }
     }
 
     final override func stopLoading() {
-        // TODO: Convert to Swift 6 Safe Type
-        /*
-        ImageURLProtocol.queue.async {
-            if self.cancelledOrComplete == false, let cancelBlock = self.block {
-                cancelBlock.cancel()
-                self.cancelledOrComplete = true
-            }
+        if let asyncTask {
+            asyncTask.cancel()
         }
-         */
     }
 
     static func urlSession() -> URLSession {
         let config = URLSessionConfiguration.ephemeral
-        config.protocolClasses = [ImageURLProtocol.classForCoder()]
+        config.protocolClasses = [ImageURLAsyncProtocol.classForCoder()]
         return  URLSession(configuration: config)
     }
 
+}
+
+extension Duration {
+    static func randomSeconds(min: Double, max: Double) -> Duration {
+        return .seconds(Double.random(in: (min...max)))
+    }
 }
