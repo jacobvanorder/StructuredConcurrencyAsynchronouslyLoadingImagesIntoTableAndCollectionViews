f6cc39ffaf54f2b512969942634f11fa9b1c9578
Jacob Van Order <jacob@sushigrass.com>
2025-06-30 08:19:27 -0500

    Converted URLProtocol Usage to Repository Pattern.

---
+++ AsynchronouslyLoadingImagesIntoTableAndCollectionViews/Async Image Loading/ImageURLRepository.swift
@@ -0,0 +1,26 @@
+//
+//  ImageURLRepository.swift
+//  Async Image Loading
+//
+//  Created by Jacob Van Order on 7/1/25.
+//  Copyright © 2025 Not Apple. All rights reserved.
+//
+
+import UIKit
+
+/// A protocol used to load an image from somewhere that uses the [repository pattern](https://www.avanderlee.com/swift/repository-design-pattern/)
+public protocol ImageURLRepository: Actor {
+    func loadImage(named name: String) async throws -> UIImage
+}
+
+extension ImageURLRepository {
+    static func image(fromData data: Data) throws -> UIImage {
+        guard let validImage = UIImage(data: data) else { throw ImageURLRepositoryError.badImageData }
+        return validImage
+    }
+}
+
+enum ImageURLRepositoryError: Error {
+    case badImageData
+    case imageDataNotFound
+}
